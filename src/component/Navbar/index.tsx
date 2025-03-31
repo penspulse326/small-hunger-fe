a2b89b32dfeaff19a72197bf7d4f5fd6e3e70c79
SHIN <akiyachiumi@gmail.com>
2025-04-01 02:28:16 +0800

    feat: add Navbar styled

--- src/component/Navbar/index.tsx
+++ src/component/Navbar/index.tsx
@@ -1,25 +1,78 @@
 'use client';
 
+import { useState } from 'react';
+
+import Link from 'next/link';
+
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import LightModeIcon from '@mui/icons-material/LightMode';
-import { IconButton, Typography, useColorScheme } from '@mui/material';
+import { IconButton, useColorScheme, Link as MuiLink } from '@mui/material';
+
+import {
+  StyledAppBar,
+  NavBox,
+  LogoBox,
+  NavItemsBox,
+  ActionsBox,
+  NavLink,
+} from './styled';
 
 function Navbar() {
   const { mode, setMode } = useColorScheme();
+
+  const [isLogin, setIsLogin] = useState(false);
 
   function handleToggleTheme() {
     setMode(mode === 'light' ? 'dark' : 'light');
   }
 
   return (
-    <div>
-      <Typography color="primary" variant="h6">
-        LOGO
-      </Typography>
-      <IconButton onClick={handleToggleTheme} color="primary">
-        {mode === 'light' ? <DarkModeIcon /> : <LightModeIcon />}
-      </IconButton>
-    </div>
+    <StyledAppBar position="sticky">
+      <NavBox>
+        <Link href="/" legacyBehavior passHref>
+          <LogoBox>
+            <MuiLink color="primary" variant="body1" underline="none">
+              Small Hunger 小餓
+            </MuiLink>
+          </LogoBox>
+        </Link>
+
+        <ActionsBox>
+          <IconButton onClick={handleToggleTheme} color="primary">
+            {mode === 'light' ? <DarkModeIcon /> : <LightModeIcon />}
+          </IconButton>
+          <IconButton onClick={() => setIsLogin(!isLogin)} color="primary">
+            1
+          </IconButton>
+          <NavItemsBox>
+            {isLogin ? (
+              <>
+                <IconButton
+                  onClick={() => setIsLogin(!isLogin)}
+                  color="primary"
+                >
+                  1
+                </IconButton>
+                <IconButton
+                  onClick={() => setIsLogin(!isLogin)}
+                  color="primary"
+                >
+                  1
+                </IconButton>
+                <IconButton
+                  onClick={() => setIsLogin(!isLogin)}
+                  color="primary"
+                >
+                  1
+                </IconButton>
+              </>
+            ) : (
+              <NavLink href="/">登入/註冊</NavLink>
+            )}
+          </NavItemsBox>
+        </ActionsBox>
+      </NavBox>
+    </StyledAppBar>
   );
 }
 
---
+++ src/component/Navbar/styled.tsx
@@ -0,0 +1,83 @@
+import Link from 'next/link';
+
+import { AppBar, Box } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+export const StyledAppBar = styled(AppBar)`
+  border-bottom: 1px solid ${({ theme }) => theme.palette.divider};
+
+  background-color: ${({ theme }) => theme.palette.background.paper};
+  box-shadow: none;
+`;
+
+export const NavBox = styled(Box)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  margin: 0 auto;
+  padding: ${({ theme }) => theme.spacing(1)};
+  width: 100%;
+  max-width: ${({ theme }) => theme.breakpoints.values.lg}px;
+`;
+
+export const LogoBox = styled(Box)`
+  display: flex;
+  align-items: center;
+
+  padding: 0 ${({ theme }) => theme.spacing(1)};
+
+  cursor: pointer;
+`;
+
+export const NavItemsBox = styled(Box)`
+  display: flex;
+  align-items: center;
+  gap: ${({ theme }) => theme.spacing(2)};
+
+  ${({ theme }) => theme.breakpoints.down('md')} {
+    display: none;
+  }
+`;
+
+export const ActionsBox = styled(Box)`
+  display: flex;
+  align-items: center;
+  gap: ${({ theme }) => theme.spacing(1)};
+`;
+
+export const NavLink = styled(Link)`
+  position: relative;
+
+  display: block;
+
+  padding: ${({ theme }) => theme.spacing(1, 2)};
+
+  color: ${({ theme }) => theme.palette.primary.main};
+  text-decoration: none;
+  font-family: inherit;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.palette.action.hover};
+  }
+
+  &::after {
+    content: '';
+
+    position: absolute;
+    bottom: 4px;
+    left: 50%;
+
+    width: 0;
+    height: 2px;
+
+    background-color: ${({ theme }) => theme.palette.primary.main};
+
+    transition: all 0.2s ease-in-out;
+    transform: translateX(-50%);
+  }
+
+  &:hover::after {
+    width: calc(100% - ${({ theme }) => theme.spacing(4)});
+  }
+`;
